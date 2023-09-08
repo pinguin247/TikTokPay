6fb434988448181daebb02a5b31901674a633c9d
Jing Qiang <tohjingqiang@gmail.com>
2023-09-09 02:16:15 +0800

    add months tab to transactions screen

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,14 +1,20 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme, Button, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; 
-import Colors from '../../constants/Colors';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import {
+  Pressable,
+  useColorScheme,
+  Button,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Colors from "../../constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -20,14 +26,15 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-       tabBarActiveTintColor: 'rgba(57, 118, 132)',
-       tabBarInactiveTintColor: 'rgba(57, 118, 132, 0.5)', 
-       tabBarStyle: { display: 'none'},
-      }}>
+        tabBarActiveTintColor: "rgba(57, 118, 132)",
+        tabBarInactiveTintColor: "rgba(57, 118, 132, 0.5)",
+        tabBarStyle: { display: "none" },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerShown: false, // Hide the header for this screen
           // headerRight: () => (
@@ -49,52 +56,66 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Transactions',
+          title: "Transactions",
           headerStyle: {
-            backgroundColor: 'white', // Change the top bar background color
+            backgroundColor: "white", // Change the top bar background color
             height: 80,
             borderBottomWidth: 0,
           },
           headerTitleStyle: {
             fontSize: 20, // Change the font size
-            color: 'black', // Change the text color
+            color: "black", // Change the text color
           },
-          headerTitleAlign: 'center',
+          headerTitleAlign: "center",
           headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()}  style={{ marginLeft: 10, marginTop: 7}} >
-              <FontAwesome name="chevron-left" size={20} color="rgb(57, 118, 132)" />
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ marginLeft: 10, marginTop: 7 }}
+            >
+              <FontAwesome
+                name="chevron-left"
+                size={20}
+                color="rgb(57, 118, 132)"
+              />
             </TouchableOpacity>
           ),
-          headerRight: () => (
-            <View style={{ width:50,flexDirection: 'row', marginRight: 15, marginTop: 7, justifyContent:"space-between" }}>
-              <TouchableOpacity onPress={() => navigation.goBack()}>
-                <FontAwesome name="search" size={20} color="rgb(102, 102, 102)" />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.goBack()}>
-                <FontAwesome name="sliders" size={20} color="rgb(102, 102, 102)" />
-              </TouchableOpacity>
-            </View>
-          ),
+          // headerRight: () => (
+          //   <View style={{ width:50,flexDirection: 'row', marginRight: 15, marginTop: 7, justifyContent:"space-between" }}>
+          //     <TouchableOpacity onPress={() => navigation.goBack()}>
+          //       <FontAwesome name="search" size={20} color="rgb(102, 102, 102)" />
+          //     </TouchableOpacity>
+          //     <TouchableOpacity onPress={() => navigation.goBack()}>
+          //       <FontAwesome name="sliders" size={20} color="rgb(102, 102, 102)" />
+          //     </TouchableOpacity>
+          //   </View>
+          // ),
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <Tabs.Screen
         name="transfer"
         options={{
-          title: 'Transfer',
+          title: "Transfer",
           headerStyle: {
-            backgroundColor: 'white', // Change the top bar background color
+            backgroundColor: "white", // Change the top bar background color
             height: 80,
             borderBottomWidth: 0,
           },
           headerTitleStyle: {
             fontSize: 20, // Change the font size
-            color: 'black', // Change the text color
+            color: "black", // Change the text color
           },
-          headerTitleAlign: 'center',
+          headerTitleAlign: "center",
           headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()}  style={{ marginLeft: 10, marginTop: 7}} >
-              <FontAwesome name="chevron-left" size={20} color="rgb(57, 118, 132)" />
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ marginLeft: 10, marginTop: 7 }}
+            >
+              <FontAwesome
+                name="chevron-left"
+                size={20}
+                color="rgb(57, 118, 132)"
+              />
             </TouchableOpacity>
           ),
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
@@ -103,27 +124,32 @@ export default function TabLayout() {
       <Tabs.Screen
         name="topup"
         options={{
-          title: 'Top Up',
+          title: "Top Up",
           headerStyle: {
-            backgroundColor: 'white', // Change the top bar background color
+            backgroundColor: "white", // Change the top bar background color
             height: 80,
             borderBottomWidth: 0,
           },
           headerTitleStyle: {
             fontSize: 20, // Change the font size
-            color: 'black', // Change the text color
+            color: "black", // Change the text color
           },
-          headerTitleAlign: 'center',
+          headerTitleAlign: "center",
           headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()}  style={{ marginLeft: 10, marginTop: 7}} >
-              <FontAwesome name="chevron-left" size={20} color="rgb(57, 118, 132)" />
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ marginLeft: 10, marginTop: 7 }}
+            >
+              <FontAwesome
+                name="chevron-left"
+                size={20}
+                color="rgb(57, 118, 132)"
+              />
             </TouchableOpacity>
           ),
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
     </Tabs>
-
-    
   );
 }
--- frontend/app/(tabs)/two.tsx
+++ frontend/app/(tabs)/two.tsx
@@ -1,15 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import ListInfo from '../../components/ListInfo';
-import { Text, View } from '../../components/Themed';
-import globalStyles from '../../constants/styles';
+import ListInfo from "../../components/ListInfo";
+// import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import globalStyles from "../../constants/styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react";
+
+const data = {
+  months: ["AUG 2023", "SEP 2023", "OCT 2023"],
+};
 
 export default function TabTwoScreen() {
+  const [selectedMonth, setSelectedMonth] = useState<string>(data.months[0]);
+
   return (
     <View style={globalStyles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <ListInfo path="app/(tabs)/two.tsx" />
+      {/* Months Selector */}
+      <View style={styles.monthsTabs}>
+        {data.months.map((month) => (
+          <TouchableOpacity onPress={() => setSelectedMonth(month)}>
+            <Text
+              style={[
+                styles.monthsTab,
+                selectedMonth === month && styles.monthsTabHighlighted,
+              ]}
+            >
+              {month}
+            </Text>
+            {selectedMonth === month && (
+              <View style={styles.highlightedColorPill} />
+            )}
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+
+      {/* Filter transaction data according to months */}
+
+      {/* <ListInfo path="app/(tabs)/two.tsx" /> */}
     </View>
   );
 }
@@ -17,11 +50,34 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
-    marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "100%",
+    boxShadow: "0px 4px 4px #397684",
+  },
+  monthsTabs: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 10,
+  },
+  monthsTab: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#858585",
+  },
+  monthsTabHighlighted: {
+    color: "black",
+  },
+  highlightedColorPill: {
+    backgroundColor: "#FF0050",
+    height: 12,
+    width: "130%",
+    transform: "translateY(-70%) translateX(-12%)",
+    opacity: 0.2,
+    borderRadius: 20,
   },
 });
