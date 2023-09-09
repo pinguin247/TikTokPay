60b165561f9dd97980062799f23bdd6c6a7708ca
Jing Qiang <tohjingqiang@gmail.com>
2023-09-09 19:07:23 +0800

    feat: add topup screen ui

--- frontend/app/(tabs)/topup.tsx
+++ frontend/app/(tabs)/topup.tsx
@@ -3,17 +3,43 @@ import { StyleSheet } from "react-native";
 import ListInfo from "../../components/ListInfo";
 import { Text, View } from "../../components/Themed";
 import globalStyles from "../../constants/styles";
+import TransferListInfo from "../../components/TransferListInfo";
 
 export default function TopUpScreen() {
   return (
     <View style={globalStyles.container}>
-      <Text style={styles.title}>Top up</Text>
+      {/* Separator */}
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <ListInfo path="app/(tabs)/transactions.tsx" />
+
+      {/* Ways to Topup */}
+      <View style={styles.transferSection}>
+        <Text style={styles.transferSectionHeader}>Ways to Topup</Text>
+        <View
+          style={styles.transferSectionSeparator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+
+        {/* Transfer Options */}
+        <TransferListInfo
+          icon="phone-settings"
+          iconColor="#7D1879"
+          iconBgColor="#ECDCEB"
+          label="Using PayNow"
+          nextScreenName="home"
+        />
+        <TransferListInfo
+          icon="bank"
+          iconColor="#386F3D"
+          iconBgColor="#E1E9E2"
+          label="From your Bank account"
+          nextScreenName="home"
+        />
+      </View>
     </View>
   );
 }
@@ -24,8 +50,23 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   separator: {
-    marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: "100%",
+    boxShadow: "0px 4px 4px #397684",
+  },
+  transferSection: {
+    alignSelf: "flex-start",
+    marginTop: 24,
+    paddingHorizontal: 24,
+    width: "100%",
+  },
+  transferSectionHeader: {
+    color: "#858585",
+    fontWeight: "600",
+  },
+  transferSectionSeparator: {
+    height: 2,
+    width: "100%",
+    marginTop: 4,
   },
 });
