6c0817e2a7e2a3e97d4aadb7e51729a0ac8b4a48
Xu Yinfeng <yinfeng.xu@kepinfra.com>
2023-09-09 22:49:12 +0800

    login function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -37,3 +37,31 @@ export const signupByEmail = async (req: express.Request, res: express.Response)
         res.sendStatus(400);
     }
 }
+
+
+export const loginByEmail = async (req: express.Request, res: express.Response) => {
+    try {
+        const idToken = req.body.token;
+
+        // Verify the ID token
+        const decodedToken = await auth.verifyIdToken(idToken);
+        const uid = decodedToken.uid;
+
+        // Fetch user information from Firestore
+        const userRef = db.collection('users').doc(uid);
+        const userDoc = await userRef.get();
+
+        if (!userDoc.exists) {
+            res.status(404).send('User not found in Firestore.');
+            return;
+        }
+
+        const userData = userDoc.data();
+
+        // Return user data to the client
+        res.status(200).json(userData);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(400);
+    }
+};
